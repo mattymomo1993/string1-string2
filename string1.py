b10addaa34b90d183ae92c1a891ce6f390447a8e
Matthew Morris <matthewjmorris1993@gmail.com>
2020-09-07 00:48:53 -0400

    finally

--- string1.py
+++ string1.py
@@ -4,7 +4,7 @@ Kenzie assignment: String1
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Matthew Morris and the demo where i got stuck to help me"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -29,7 +29,10 @@ __author__ = "???"
 
 def donuts(count):
     # your code here
-    return
+    if count < 10:
+        return f"Number of donuts: {count}"
+    else:
+        return "Number of donuts: many"
 
 
 # B. both_ends
@@ -43,7 +46,10 @@ def donuts(count):
 
 def both_ends(s):
     # your code here
-    return
+    if len(s) < 2:
+        return ''
+
+    return s[0:2] + s[-2:]
 
 
 # C. fix_start
@@ -59,9 +65,7 @@ def both_ends(s):
 
 def fix_start(s):
     # your code here
-    return
-
-
+    return s[0] + s[1:].replace(s[0], '*')
 # D. mix_up
 # Given strings a and b, return a single string with a and
 # b separated by a space '<a> <b>', except swap the first
@@ -74,7 +78,7 @@ def fix_start(s):
 
 def mix_up(a, b):
     # your code here
-    return
+    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
